9d13baae368b188d33e4d331a8a596cd1b8ee4c7
presstab <presstab1337@gmail.com>
2017-09-24 13:46:38 -0600

    more share refactor for .ico

--- share/qt/make_windows_icon.sh
+++ share/qt/make_windows_icon.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 # create multiresolution windows icon
 #mainnet
-ICON_SRC=../../src/qt/res/icons/bitcoin.png
-ICON_DST=../../src/qt/res/icons/bitcoin.ico
-convert ${ICON_SRC} -resize 16x16 bitcoin-16.png
-convert ${ICON_SRC} -resize 32x32 bitcoin-32.png
-convert ${ICON_SRC} -resize 48x48 bitcoin-48.png
-convert bitcoin-16.png bitcoin-32.png bitcoin-48.png ${ICON_DST}
+ICON_SRC=../../src/qt/res/icons/hyperstake.png
+ICON_DST=../../src/qt/res/icons/hyperstake.ico
+convert ${ICON_SRC} -resize 16x16 hyperstake-16.png
+convert ${ICON_SRC} -resize 32x32 hyperstake-32.png
+convert ${ICON_SRC} -resize 48x48 hyperstake-48.png
+convert hyperstake-16.png hyperstake-32.png hyperstake-48.png ${ICON_DST}
 #testnet
-ICON_SRC=../../src/qt/res/icons/bitcoin_testnet.png
-ICON_DST=../../src/qt/res/icons/bitcoin_testnet.ico
-convert ${ICON_SRC} -resize 16x16 bitcoin-16.png
-convert ${ICON_SRC} -resize 32x32 bitcoin-32.png
-convert ${ICON_SRC} -resize 48x48 bitcoin-48.png
-convert bitcoin-16.png bitcoin-32.png bitcoin-48.png ${ICON_DST}
+ICON_SRC=../../src/qt/res/icons/hyperstake.png
+ICON_DST=../../src/qt/res/icons/hyperstake.ico
+convert ${ICON_SRC} -resize 16x16 hyperstake-16.png
+convert ${ICON_SRC} -resize 32x32 hyperstake-32.png
+convert ${ICON_SRC} -resize 48x48 hyperstake-48.png
+convert hyperstake-16.png hyperstake-32.png hyperstake-48.png ${ICON_DST}
 rm bitcoin-16.png bitcoin-32.png bitcoin-48.png
